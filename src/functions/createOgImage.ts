881a0988d8427730dc34ff3bbcf382b8caa94a97
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2023-11-08 01:59:04 -0300

    modify: createOGImage

--- src/functions/createOgImage.ts
+++ src/functions/createOgImage.ts
@@ -1,16 +1,6 @@
 import { Handler } from '@netlify/functions'
 import { chromium, type Page } from 'playwright'
 
-const isLocal = import.meta.env.MODE === 'development'
-const LOCAL_CHROME_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-const LOCAL_URL = 'http://localhost:4321'
-
-const getConfig = async () => {
-  const executablePath = isLocal ? LOCAL_CHROME_PATH : await chromium.executablePath
-  const url = isLocal ? LOCAL_URL : import.meta.env.SITE
-  return { executablePath, url }
-}
-
 const returnImage = (buffer) => ({
   headers: {
     'Content-Type': 'image/jpg'
@@ -20,33 +10,23 @@ const returnImage = (buffer) => ({
   isBase64Encoded: true,
 })
 
-export const handler: Handler = async (event) => {
-
+const handler: Handler = async (event) => {
+  const url = 'https://cuando-juega-edlp.netlify.app'
   const browser = await chromium.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
+    headless: true
   })
-
-  const { executablePath, url } = await getConfig()
-
   const page: Page = await browser.newPage()
-
-  await page.setViewport({
+  await page.setViewportSize({
     width: 1200,
-    height: 800,
-    deviceScaleFactor: 2
+    height: 800
   })
-
   await page.goto(url)
-
-  const el = await page.$('body')
-
+  const el = await page.$('#App')
   const screenshot = await el.screenshot()
-
   await browser.close()
-
   return returnImage(screenshot)
+}
+
+export {
+  handler
 }
