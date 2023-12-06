0396c3a58bed3c26e8306153dc2c34486a1b37c3
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2023-12-06 12:26:06 -0300

    fix: added problem with Array Partidos is minor to 0

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,3 +1,3 @@
 const isLocal = import.meta.env.MODE === 'development'
 const baseUrlDev = 'http://localhost:4321'
-export const URL_API_ENDPOINT_2023 = isLocal ? `${baseUrlDev}/api/2023/partidos` : `${import.meta.env.SITE}/api/2023/partidos`
+export const URL_API_ENDPOINT = (year: string) => isLocal ? `${baseUrlDev}/api/${year}/partidos` : `${import.meta.env.SITE}/api/${year}/partidos`
