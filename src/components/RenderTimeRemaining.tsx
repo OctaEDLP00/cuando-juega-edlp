83cf640f45a1537dfc8decffa363545b63ae1e57
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-01-28 05:16:36 -0300

    refactor: re formatted of the file

--- src/components/RenderTimeRemaining.tsx
+++ src/components/RenderTimeRemaining.tsx
@@ -6,23 +6,17 @@ export function RenderTimeRemaining({ days, hours, minutes, seconds }: TRemainin
   }
 
   if (Number(days) !== 0) {
-    return <span className='text-center text-2xl'>
+    return <span className='text-center'>
       {
-        Number(days) === 1 ? (
-          <>
-            {Number(days).toString()} Día {hours}:{minutes}:{seconds}
-          </>
-        ) : (
-          <>
-            {Number(days).toString()} Dias {hours}:{minutes}:{seconds}
-          </>
-        )
+        Number(days) === 1
+          ? (<>{days} Día {hours}:{minutes}:{seconds}</>)
+          : (<>{days} Dias {hours}:{minutes}:{seconds}</>)
       }
     </span>
   }
 
-  if (Number(days) === 0 && Number(hours) !== 0 && Number(minutes) !== 0 && Number(seconds) !== 0) {
-    return <span className='text-center text-2xl'>
+  if (Number(hours) >= 0 && Number(minutes) >= 0 && Number(seconds) !== 0) {
+    return <span className='text-center'>
       {hours}:{minutes}:{seconds}
     </span>
   }
