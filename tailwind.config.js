b09ba8a206daf350b4d56c147257821c26e628c5
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-01-28 04:57:38 -0300

    refactor: tailwind config change cjs to js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
-module.exports = {
-  darkMode: 'class',
-	content: [
-    './src/**/*.{astro,js,jsx,md,mdx,ts,tsx}'
-  ],
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [ './src/**/*.{astro,js,jsx,md,mdx,ts,tsx}' ],
   theme: {
     fontFamily: {
       CCPL: ['Cascadia Code PL', ...defaultTheme.fontFamily.mono]
