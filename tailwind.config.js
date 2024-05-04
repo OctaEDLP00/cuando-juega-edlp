d5fc511c05a26e13e902708ee2e51242e1c6521b
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-05-04 07:45:52 -0300

    add more rules of skew in tailwind file config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
 	content: [ './src/**/*.{astro,js,jsx,md,mdx,ts,tsx}' ],
   theme: {
-    fontFamily: {
+    extend: {
+      skew: {
+        '45': '45deg',
+        '90': '90deg',
+        '135': '135deg',
+        '180': '180deg',
+        '225': '225deg',
+        '270': '270deg'
+      }
+		},
+		fontFamily: {
       CCPL: ['Cascadia Code PL', ...defaultTheme.fontFamily.mono]
     }
 	}
