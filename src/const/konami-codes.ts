5712222c198d9226df895df802777d0a49529cce
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-05-04 07:54:42 -0300

    add logical of the Easter egg in this files

---
+++ src/const/konami-codes.ts
@@ -0,0 +1,50 @@
+export const konamiCodes = {
+	edlp: [
+		'e',
+		'd',
+		'l',
+		'p',
+		'9',
+		'1',
+		'1'
+	],
+	lali: [
+		'ArrowUp',
+		'ArrowUp',
+		'ArrowDown',
+		'ArrowDown',
+		'ArrowLeft',
+		'ArrowRight',
+		'ArrowLeft',
+		'ArrowRight',
+		'b',
+		'a'
+	],
+	edlpCursor: [
+		'e',
+		's',
+		't',
+		'u',
+		'd',
+		'i',
+		'a',
+		'n',
+		't',
+		'e',
+		's',
+	],
+	laliCursor: [
+		'l',
+		'i',
+		'b',
+		'e',
+		'r',
+		't',
+		'a',
+		'd',
+		'o',
+		'r',
+		'e',
+		's'
+	],
+}
---
+++ src/utils/Konami.ts
@@ -0,0 +1,56 @@
+interface KonamiProps {
+	code: Array<string>
+	keyPressed: string
+	onFinishTime?: number
+	onSucces: () => void
+	onFinish?: () => void
+}
+
+interface KonamiState {
+	[key: string]: number
+}
+
+const konamiState: KonamiState = {}
+
+/**
+ * @description
+ * This function is a simple implementation of the Konami code.
+ *
+ * @param {Array<string>} code The Konami code
+ * @param {string} keyPressed The key pressed by the user
+ * @param {number} onFinishTime The time to wait before calling onFinish
+ * @param {Function} onSucces The function to call when the Konami code is successfully entered
+ * @param {Function} onFinish The function to call when the Konami code is not successfully entered
+ * @returns {void}
+ */
+export function Konami({ code, keyPressed, onFinishTime, onSucces, onFinish }: KonamiProps): void {
+	const codeKey = code.join('-')
+	const konamiCodePosition = konamiState[codeKey]
+
+	const actualKey = keyPressed.toLowerCase()
+	const actualCode = code[konamiCodePosition].toLowerCase()
+
+	if (actualKey !== actualCode) {
+		if (onFinish) onFinish()
+		konamiState[codeKey] = 0
+		return
+	}
+
+	const nextPosition = konamiCodePosition + 1
+
+	if (nextPosition === code.length) {
+		onSucces()
+		konamiState[codeKey] = 0
+
+		if (onFinishTime && onFinish) {
+			setTimeout(
+				() => {
+					onFinish()
+				},
+				onFinishTime ?? 1000
+			)
+		}
+	} else {
+		konamiState[codeKey] = nextPosition
+	}
+}
