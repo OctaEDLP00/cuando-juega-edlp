b080c3d71f65f570ab2d768eebef2faa768ece3c
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-05-13 05:32:33 -0300

    add: new string type copa "torneo-de-la-liga"

--- src/pages/api/partido/[copa].ts
+++ src/pages/api/partido/[copa].ts
@@ -1,12 +1,10 @@
 import { partidos } from '@const/partidos'
 import { type APIRoute } from 'astro'
 
-type COPA = "copa-de-la-liga" | "copa-libertadores" | "copa-sudamericana" | "copa-argentina" | "noche-del-león" | "supercopa-argentina"
+type Copa = "copa-de-la-liga" | "copa-libertadores" | "copa-sudamericana" | "copa-argentina" | "noche-del-león" | "supercopa-argentina" | "torneo-de-la-liga"
 
 export const GET: APIRoute = ({ params }) => {
-	const { copa } = params as { copa: COPA }
-	const paritdoPorCopa = partidos.filter((partido) => {
-		return partido.copa.replaceAll(' ', '-').toLowerCase() === copa
-	})
-	return new Response(JSON.stringify(paritdoPorCopa))
+	const { copa } = params as { copa: Copa }
+	const partidoPorCopa = partidos.filter(({ copa: COPA }) => COPA.replaceAll(' ', '-').toLowerCase() === copa)
+	return new Response(JSON.stringify(partidoPorCopa))
 }
