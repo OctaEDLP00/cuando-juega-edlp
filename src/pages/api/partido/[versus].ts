8c24c42d17918b92d6ff8135da2e4a0228bd62ea
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-05-13 05:28:16 -0300

    delete: dynamic route visior and versus by bug in astro

--- src/pages/api/partido/[versus].ts
+++
@@ -1,9 +0,0 @@
-import { partidos } from '@const/partidos'
-import { type APIRoute } from 'astro'
-import { sanitizeVs } from '@utils/index'
-
-export const GET: APIRoute = ({ params }) => {
-	const { versus } = params
-	const matchVersus = partidos.find(({ vs }) => sanitizeVs(vs) === versus)
-	return new Response(JSON.stringify(matchVersus))
-}
