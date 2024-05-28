5a32ee7037e3d927ff742d4b7c0af386820e95c2
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-05-28 01:58:27 -0300

    fix(index.ts): add types references of the edge-runtime and change comments eslint rules

--- supabase/functions/create-image/index.ts
+++ supabase/functions/create-image/index.ts
@@ -1,20 +1,26 @@
-// eslint-disanle-next-line @typescript-eslint/ban-ts-comment
+/* eslint-disable indent */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disanle-next-line @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import { serve } from 'https://deno.land/std@0.224.0/http/server.ts'
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+///  <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
 
-/* eslint-disable */
-const SUPABASE_URL = Deno.env.get('PUBLIC_SUPABASE_URL') || ''
+// eslint-disable-next-line no-undef
+const SUPABASE_URL = Deno.env.get('SUPABASE_URL') || ''
+// eslint-disable-next-line no-undef
 const SUPABASE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || ''
-/* eslint-enable */
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 const json = (data: object, { status = 200 }) => {
-	return new Response(JSON.stringify(data), {
-		headers: { 'Content-Type': 'application/json' },
-		status
-	})
+	return new Response(
+		JSON.stringify(data),
+		{
+			headers: { 'Content-Type': 'application/json' },
+			status
+		}
+	)
 }
 
 serve(async (req: Request) => {
@@ -26,7 +32,7 @@ serve(async (req: Request) => {
 		return json({ message: 'Image already set' })
 	}
 
-	const res = await fetch(`https://cuando-juega-edlp.netlify.app/.netlify/functions/createOgImage`)
+	const res = await fetch('https://cuando-juega-edlp.netlify.app/.netlify/functions/createOgImage')
 
 	if (!res.ok) {
 		console.error('Error generating image')
@@ -59,3 +65,15 @@ serve(async (req: Request) => {
 
 	return json({ message: 'Image generated' }, { status: 200 })
 })
+
+/* To invoke locally:
+
+	1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+	2. Make an HTTP request:
+
+	curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/createImage' \
+		--header 'Authorization: Bearer ' \
+		--header 'Content-Type: application/json' \
+		--data '{"name":"Functions"}'
+
+*/
