1d4ad1ada3849683ad79daf93a581f9b204dc91a
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-06-01 08:19:03 -0300

    fix: change import supabase version and delete import serve, deleted someone comments and other changes

--- supabase/functions/create-image/index.ts
+++ supabase/functions/create-image/index.ts
@@ -1,21 +1,15 @@
-/* eslint-disable indent */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-/* eslint-disanle-next-line @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-import { serve } from 'https://deno.land/std@0.224.0/http/server.ts'
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-///  <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
+/* eslint-disable no-undef */
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.43.2'
+/// <reference types='https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts' />
 
-// eslint-disable-next-line no-undef
 const SUPABASE_URL = Deno.env.get('SUPABASE_URL') || ''
-// eslint-disable-next-line no-undef
 const SUPABASE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') || ''
-
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-const json = (data: object, { status = 200 }) => {
-	return new Response(
-		JSON.stringify(data),
+const { log, error } = console
+
+function json(data: object, { status = 200 }: { status?: number | undefined }) {
+	return new Response(JSON.stringify(data),
 		{
 			headers: { 'Content-Type': 'application/json' },
 			status
@@ -23,19 +17,20 @@ const json = (data: object, { status = 200 }) => {
 	)
 }
 
-serve(async (req: Request) => {
+// eslint-disable-next-line no-undef
+Deno.serve(async (req: Request) => {
 	const reqJson = await req.json()
 	const { type, record } = reqJson
 	const { image } = record ?? {}
 
 	if (type === 'UPDATE' && Boolean(image)) {
-		return json({ message: 'Image already set' })
+		return json({ message: 'Image already set' }, { status: 409 })
 	}
 
 	const res = await fetch('https://cuando-juega-edlp.netlify.app/.netlify/functions/createOgImage')
 
 	if (!res.ok) {
-		console.error('Error generating image')
+		error('Error generating image')
 		return json({ message: 'Error generating image' }, { status: 400 })
 	}
 
@@ -45,11 +40,11 @@ serve(async (req: Request) => {
 		.from('image-og')
 		.upload('og.png', arrayBuffer, { contentType: 'image/png', upsert: true })
 
-	console.log(uploadedImage)
+	log(uploadedImage)
 
 	if (errorUploadingImage) {
-		console.error('Error uploading image')
-		console.error(errorUploadingImage)
+		error('Error uploading image')
+		error(errorUploadingImage)
 		return json({ message: 'Error uploading image' }, { status: 400 })
 	}
 
@@ -71,7 +66,7 @@ serve(async (req: Request) => {
 	1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
 	2. Make an HTTP request:
 
-	curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/createImage' \
+	curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/create-image' \
 		--header 'Authorization: Bearer ' \
 		--header 'Content-Type: application/json' \
 		--data '{"name":"Functions"}'
