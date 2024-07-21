33415f359be7b2de978476024acf8dea1281c5d6
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-07-21 08:24:40 -0300

    chore(utils): move functionality konami use the paradigm POO

--- src/utils/Konami.ts
+++ src/utils/Konami.ts
@@ -1,46 +1,101 @@
 import type { KonamiProps, KonamiState } from '@const/types'
 
+/** @const {KonamiState} konamiState */
 const konamiState: KonamiState = {}
 
 /**
- * @description
- * This function is a simple implementation of the Konami code.
- *
- * @param {Array<string>} code The Konami code
- * @param {string} keyPressed The key pressed by the user
- * @param {number} onFinishTime The time to wait before calling onFinish
- * @param {function} onSucces The function to call when the Konami code is successfully entered
- * @param {function} onFinish The function to call when the Konami code is not successfully entered
- * @returns {void}
+ * @module Konami
+ * @description This class is a simple implementation of the Konami code.
+ * @class Konami
+ * @implements {IKonami}
+ * @property {Array<string>} code
+ * @property {string} keyPressed
+ * @property {number} [onFinishTime] If onFinishTime is not define, it's default value is 1000ms
+ * @property {function} onSucces
+ * @property {function} [onFinish]
  */
-export function Konami({ code, keyPressed, onFinishTime, onSucces, onFinish }: KonamiProps): void {
-	const codeKey = code.join('-')
-	const konamiCodePosition = konamiState[codeKey] || 0
+export class Konami {
 
-	const actualKey = keyPressed.toLowerCase()
-	const actualCode = code[konamiCodePosition].toLowerCase()
+	/** An array of the letters that will be the keys pressed to execute the easter egg */
+	code: Array<string>
+	/** the key pressed by the user */
+	keyPressed: string
+	/**
+	 * If onFinishTime is not define, it's default value is 1000ms
+	 * @default '1000ms'
+	 */
+	onFinishTime?: number
+	/**  */
+	onSucces: () => void
+	/**  */
+	onFinish?: () => void
 
-	if (actualKey !== actualCode) {
-		if (onFinish) onFinish()
-		konamiState[codeKey] = 0
-		return
+	/**
+	 * this a description constructor
+	 * @constructor
+	 * @param {import('@const/types').KonamiProps} konamiProps
+	 */
+	constructor({ code, keyPressed, onSucces, onFinish, onFinishTime }: KonamiProps) {
+		this.code = code
+		this.keyPressed = keyPressed
+		this.onFinishTime = onFinishTime
+		this.onSucces = onSucces
+		this.onFinish = onFinish
+		this.init(
+			this.code,
+			this.keyPressed,
+			this.onFinishTime,
+			this.onSucces,
+			this.onFinish
+		)
 	}
 
-	const nextPosition = konamiCodePosition + 1
+/**
+ * @param {Array<string>} code
+ * @param {string} keyPressed
+ * @param {() => void} onFinish
+ * @param {() => void} onSucces
+ * @param {number} onFinishTime
+ * @return {void}
+ */
+	private init(
+		code: Array<string>,
+		keyPressed: string,
+		onFinishTime: number,
+		onSucces: () => void,
+		onFinish: () => void
+	): void {
+		const codeKey = code.join('-')
+		const konamiCodePosition = konamiState[codeKey] || 0
 
-	if (nextPosition === code.length) {
-		onSucces()
-		konamiState[codeKey] = 0
+		const actualKey = keyPressed.toLowerCase()
+		const actualCode = code[konamiCodePosition].toLowerCase()
 
-		if (onFinishTime && onFinish) {
-			setTimeout(
-				() => {
-					onFinish()
-				},
-				onFinishTime ?? 1000
-			)
+		if (actualKey !== actualCode) {
+			if (onFinish) onFinish()
+			konamiState[codeKey] = 0
+			return
 		}
-	} else {
-		konamiState[codeKey] = nextPosition
+
+		const nextPosition = konamiCodePosition + 1
+
+		if (nextPosition === code.length) {
+			onSucces()
+			konamiState[codeKey] = 0
+
+			if (onFinishTime && onFinish) {
+				setTimeout(
+					() => {
+						onFinish()
+					},
+					onFinishTime ?? 1000
+				)
+			}
+		} else {
+			konamiState[codeKey] = nextPosition
+		}
 	}
+
 }
+
+export default Konami
