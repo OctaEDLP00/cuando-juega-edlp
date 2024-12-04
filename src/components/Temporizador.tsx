90e0e684d0b5f83af4db96f8317e590bbd02368e
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-12-04 07:29:50 -0300

    Actualizar Temporizador.tsx

--- src/components/Temporizador.tsx
+++ src/components/Temporizador.tsx
@@ -17,7 +17,7 @@ export default function Card({ date, copa, isSuspended }: Partidos) {
 
 	if (date === null) {
 		return (
-			<div id='info' className={`${cn(copa === 'Torneo de La Liga' && 'bg-[#1c4b88] dark:bg-[#4394ff]', copa === 'Noche del León' && 'bg-[#3730A3] dark:bg-[#6366F1]', copa === 'Copa Argentina' && 'bg-[#FF5722] dark:bg-[#FF9800]', copa === 'Copa de La Liga' && 'bg-[#09798C] dark:bg-[#0BBAD9]', copa === 'Copa Libertadores' && 'bg-[#007848] dark:bg-[#4AA02C]', copa === 'Copa Sudamericana' && 'bg-[#A10102] dark:bg-[#FF1010]', copa === 'Supercopa Argentina' && 'bg-[#8b1578] dark:bg-[#B93CA5]')} rounded-b-md py-2 px-4 flex flex-col justify-center w-full`}>
+			<div id='info' className={`${cn(copa === 'Torneo de La Liga' && 'bg-[#1c4b88] dark:bg-[#4394ff]', copa === 'Noche del León' && 'bg-[#3730A3] dark:bg-[#6366F1]', copa === 'Copa Argentina' && 'bg-[#FF5722] dark:bg-[#FF9800]', copa === 'Copa de La Liga' && 'bg-[#09798C] dark:bg-[#0BBAD9]', copa === 'Copa Libertadores' && 'bg-[#007848] dark:bg-[#4AA02C]', copa === 'Copa Sudamericana' && 'bg-[#A10102] dark:bg-[#FF1010]', copa === 'Supercopa Argentina' && 'bg-[#8b1578] dark:bg-[#B93CA5]', copa === 'Trofeo de Campeones' && '')} rounded-b-md py-2 px-4 flex flex-col justify-center w-full`}>
         <span className='text-center uppercase'>
           Sin info
         </span>
@@ -27,7 +27,7 @@ export default function Card({ date, copa, isSuspended }: Partidos) {
 
 	if ((date === null || date !== null) && isSuspended && isSuspended !== null) {
 		return (
-			<div id='info' className={`${cn(copa === 'Torneo de La Liga' && 'bg-[#1c4b88] dark:bg-[#4394ff]', copa === 'Noche del León' && 'bg-[#3730A3] dark:bg-[#6366F1]', copa === 'Copa Argentina' && 'bg-[#FF5722] dark:bg-[#FF9800]', copa === 'Copa de La Liga' && 'bg-[#09798C] dark:bg-[#0BBAD9]', copa === 'Copa Libertadores' && 'bg-[#007848] dark:bg-[rgb(74,160,44)]', copa === 'Copa Sudamericana' && 'bg-[#A10102] dark:bg-[#FF1010]', copa === 'Supercopa Argentina' && 'bg-[#8b1578] dark:bg-[#B93CA5]')} rounded-b-md py-2 px-4 flex flex-col justify-center w-full`}>
+			<div id='info' className={`${cn(copa === 'Torneo de La Liga' && 'bg-[#1c4b88] dark:bg-[#4394ff]', copa === 'Noche del León' && 'bg-[#3730A3] dark:bg-[#6366F1]', copa === 'Copa Argentina' && 'bg-[#FF5722] dark:bg-[#FF9800]', copa === 'Copa de La Liga' && 'bg-[#09798C] dark:bg-[#0BBAD9]', copa === 'Copa Libertadores' && 'bg-[#007848] dark:bg-[rgb(74,160,44)]', copa === 'Copa Sudamericana' && 'bg-[#A10102] dark:bg-[#FF1010]', copa === 'Supercopa Argentina' && 'bg-[#8b1578] dark:bg-[#B93CA5]', copa === 'Trofeo de Campeones' && '')} rounded-b-md py-2 px-4 flex flex-col justify-center w-full`}>
 				<span className='text-center uppercase'>
 					Suspendido
 				</span>
@@ -36,7 +36,7 @@ export default function Card({ date, copa, isSuspended }: Partidos) {
 	}
 
 	return (
-		<div id='info' className={`${cn(copa === 'Torneo de La Liga' && 'bg-[#1c4b88] dark:bg-[#4394ff]', copa === 'Noche del León' && 'bg-[#3730A3] dark:bg-[#6366F1]', copa === 'Copa Argentina' && 'bg-[#FF5722] dark:bg-[#FF9800]', copa === 'Copa de La Liga' && 'bg-[#09798C] dark:bg-[#0BBAD9]', copa === 'Copa Libertadores' && 'bg-[#007848] dark:bg-[#4AA02C]', copa === 'Copa Sudamericana' && 'bg-[#A10102] dark:bg-[#FF1010]', copa === 'Supercopa Argentina' && 'bg-[#8b1578] dark:bg-[#B93CA5]')} rounded-b-md py-2 px-4 flex flex-col justify-center w-full h-auto`}>{RenderTimeRemaining({ days, hours, minutes, seconds })}</div>
+		<div id='info' className={`${cn(copa === 'Torneo de La Liga' && 'bg-[#1c4b88] dark:bg-[#4394ff]', copa === 'Noche del León' && 'bg-[#3730A3] dark:bg-[#6366F1]', copa === 'Copa Argentina' && 'bg-[#FF5722] dark:bg-[#FF9800]', copa === 'Copa de La Liga' && 'bg-[#09798C] dark:bg-[#0BBAD9]', copa === 'Copa Libertadores' && 'bg-[#007848] dark:bg-[#4AA02C]', copa === 'Copa Sudamericana' && 'bg-[#A10102] dark:bg-[#FF1010]', copa === 'Supercopa Argentina' && 'bg-[#8b1578] dark:bg-[#B93CA5]', copa === 'Trofeo de Campeones' && '')} rounded-b-md py-2 px-4 flex flex-col justify-center w-full h-auto`}>{RenderTimeRemaining({ days, hours, minutes, seconds })}</div>
   )
 }
 
